0fb4006f817cf0337c9935c0db2d4811778237db
eaguidigodo <eaguidigodo@gmail.com>
2021-09-17 09:14:19 +0100

    Implemented post creation feature

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,34 @@
 class PostsController < ApplicationController
+  before_action: only: [edit, update, destroy]
   def index
+    @post = Post.new
+    @posts = Post.all
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_index_path, notice: "Your post has succesfully been created!"
+    else
+      render :index
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
+  def set_params
+    @post = Post.find(params[:id])
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
+  validates :content, presence: true
 end
