fe42602e2962dbe4254447ff9fe9e6b4764314dd
eaguidigodo <eaguidigodo@gmail.com>
2021-09-27 00:48:58 +0100

    Add avatar in profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,6 @@ class User < ApplicationRecord
     validates :password, length: { minimum: 6 }
     has_many :posts
     has_many :favorites, dependent: :destroy
+
+    mount_uploader :image, ImageUploader
 end
